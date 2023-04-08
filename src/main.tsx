f2db6fb7790cfb1fd25078dad690198e6f219414
“PedroHenrique1606” <pedromelosilva11911@gmail.com>
2023-04-08 09:26:43 -0300

    Finishing React Fundamentals 080423

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Tweet } from './components/Tweet'
+import './global.css'
+//Ferramenta de tipagem estática para o JS
+//Tipagem estática: mecanismo par evitar erros ENQUANTO estamos desenvolvendo
 
-const editUser = false;
 //Programação declarativa
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <h1>Hello, World.</h1>
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ratione asperiores deserunt, consequuntur minima velit voluptatibus pariatur obcaecati itaque expedita harum voluptates, nemo sed temporibus sunt quasi saepe dignissimos impedit.</p>
-      <button>{editUser ? 'Editar usuário' : 'Criar usuário'}</button>
-    </div>
+    <Tweet user='Pedro' likes={10}>
+      Meu primeiro tweet
+    </Tweet>
+    <Tweet user='Rodrigo'>
+      Meu primeiro tweet
+    </Tweet>
+    <Tweet user='Dante'>
+      Meu primeiro tweet
+    </Tweet>
+    <Tweet user='Ana'>
+      Meu primeiro tweet
+    </Tweet>
+
+    <Tweet user='Carina'>
+      Conteúdo 4
+    </Tweet>
   </React.StrictMode>,
 )
 //Componentes: pequenas partes de inferface reuitilizavéis. 
