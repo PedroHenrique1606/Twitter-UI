1352747039d311547bb02c056e7dc95ad4890e2e
“PedroHenrique1606” <pedromelosilva11911@gmail.com>
2023-04-26 22:21:46 -0300

    create timeline 26042023

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,18 +1,34 @@
-interface TweetProps{
-    user : string
-    children: string
-    likes?: number
-}
-const tweetStyles = {
-    borderRadius: 8
-}
+import { ArrowsClockwise, ChatCircle, Heart } from 'phosphor-react';
+import './Tweet.css';
 
-export function Tweet(props:TweetProps){
-    return (
-        <div className="tweet" style={tweetStyles}>
-            <strong>{props.user}</strong>
-            <p>{props.children}</p>
-            <button>Like {props.likes ?? 0}</button>
-        </div>
-    );
+export function Tweet(){
+    return(
+        <a href='' className="tweet">
+            <img src="https://github.com/PedroHenrique1606.png" alt="Pedro Henrique" />
+            <div className='tweet-content'>
+                <div className='tweet-content-header'>
+                    <strong>Pedro Henrique</strong>
+                    <span>@pedrohenrique1606 </span>
+                </div>
+                <p>Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram: 
+                    <br/>
+                    ✅ npm start: De 32s para 400ms (sim, demorava 30s) <br/>
+                    ✅ npm build: De 120s para 22s<br/>
+
+                    Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥
+                    </p>
+                    <div className="tweet-content-footer">
+                        <button type='button'>
+                            <ChatCircle></ChatCircle> 20
+                        </button>
+                        <button type='button'>
+                            <ArrowsClockwise></ArrowsClockwise> 30
+                        </button>
+                        <button type='button'>
+                            <Heart></Heart> 40
+                        </button>
+                    </div>
+            </div>
+        </a >
+    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import twitterLogo from './assets/logo-twitter.svg'
-import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User } from 'phosphor-react'
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
+import { Tweet }from './components/Tweet';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -46,7 +47,27 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <button className='new-tweet' type='button'>Tweet</button>
       </aside>
       <div className="content">
-        Content
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle></Sparkle>
+          </div>
+          
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/PedroHenrique1606.png" alt="Pedro Henrique" />
+              <textarea id="tweet" placeholder="What's happening?"/>
+            </label>
+
+            <button type='submit'>Tweet</button>
+          </form>
+
+          <div className='separator'></div>
+        </main>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
       </div>
     </div>
   </React.StrictMode>,
